95ef1267d0eb3a486f39c8c729eae4f2702b1eb8
max <maxime@kaliel.fr>
2025-03-24 12:39:49 +0100

    vite php refresh

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
 import {defineConfig} from "vite";
 import {glob} from 'glob';
-import path from 'node:path';
+import {relative, resolve, extname} from 'node:path';
 import {fileURLToPath} from 'node:url';
-import.meta.path;
 
+
+const phpRefreshPlugin = () => ({
+  name: "php-refresh",
+  configureServer({watcher, ws}) {
+    watcher.add(resolve(__dirname, "site/**/*.php"));
+    watcher.on("change", function (path) {
+      if (path.endsWith(".php")) {
+        ws.send({
+          type: "full-reload",
+        });
+      }
+    });
+  },
+});
 export default defineConfig({
   server: {
     port: 3000,
     host: "0.0.0.0"
   },
-  plugins: [HotReloadPhpFiles],
+  plugins: [phpRefreshPlugin()],
   build: {
     outDir: '',
     assetsDir: 'public',
@@ -20,9 +33,9 @@ export default defineConfig({
         glob.sync('assets/**/[^_]*.{js,scss}').map(file => [
           // This remove `src/` as well as the file extension from each
           // file, so e.g. src/nested/foo.js becomes nested/foo
-          path.relative(
+          relative(
             'assets',
-            file.slice(0, file.length - path.extname(file).length)
+            file.slice(0, file.length - extname(file).length)
           ),
           // This expands the relative paths to absolute paths, so e.g.
           // src/nested/foo becomes /project/src/nested/foo.js
@@ -33,20 +46,3 @@ export default defineConfig({
   }
 });
 
-export function HotReloadPhpFiles() {
-
-  return {
-    name: 'custom-hmr',
-    enforce: 'post',
-    // HMR
-    handleHotUpdate({file, server}) {
-      // only hot reload files with php extension inside 'site' folder
-      if (file.endsWith('.php')) {
-        server.ws.send({
-          type: 'full-reload',
-          path: '*'
-        });
-      }
-    },
-  }
-}
