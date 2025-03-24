5d83ce5835f39f6f89ea8027c11e759bf7ce21c3
max <maxime@kaliel.fr>
2025-03-24 14:00:07 +0100

    default assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
+/**
+ * Put only common javascript for every page here.
+ * For better performance put
+ * page specific javascript into js/templates/page-template.js
+ */
 console.log('Hello Kirby CMS');
