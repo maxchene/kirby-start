b308d8b2eb3184ccbf5bdc97f0b4b6578d2f124c
max <maxime@kaliel.fr>
2025-03-24 13:59:54 +0100

    load vite js

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script type="module" src="http://localhost:3000/@vite/client"></script>
+  <link rel="stylesheet" href="http://localhost:3000/assets/css/app.scss">
 </head>
 <body>
