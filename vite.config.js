4dfad8b9cb4a84bf71eff9b1f16f6b939c50cfa4
max <maxime@kaliel.fr>
2025-03-26 14:26:34 +0100

    vite css name in manifest

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from "vite";
 import {glob} from 'glob';
 import {relative, resolve, extname} from 'node:path';
 import {fileURLToPath} from 'node:url';
+import * as fs from "node:fs";
 
 
 const phpRefreshPlugin = () => ({
@@ -17,15 +18,31 @@ const phpRefreshPlugin = () => ({
     });
   },
 });
+
+const cssNameManifest = () => ({
+  name: 'css-manifest-name',
+  enforce: 'post',
+  writeBundle(options, bundle) {
+    const manifestPath = `${options.dir}/manifest.json`;
+    const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf8'));
+
+    for (const key in manifest) {
+      if (key.endsWith('.scss')) {
+        manifest[key].name = key.replace('assets/css/', 'css/').replace('.scss', '');
+      }
+    }
+    fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
+  }
+});
 export default defineConfig({
   server: {
     port: 3000,
     host: "0.0.0.0"
   },
-  plugins: [phpRefreshPlugin()],
+  plugins: [phpRefreshPlugin(), cssNameManifest()],
   build: {
-    outDir: '',
-    assetsDir: 'public',
+    outDir: 'public',
+    assetsDir: '',
     manifest: "manifest.json",
     cssMinify: 'lightningcss',
     rollupOptions: {
