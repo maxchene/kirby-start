e1500338c6463b73837fdeaebc6f576c38dab342
max <maxime@kaliel.fr>
2025-05-19 11:49:08 +0200

    vite plugin

--- site/plugins/vite-assets/index.php
+++ site/plugins/vite-assets/index.php
@@ -13,21 +13,27 @@ Kirby::plugin('maxchene/vite-assets', [
             return uri($url);
         }
     ],
+    'siteMethods' => [
+        'isDev' => function () {
+            return isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'dev';
+        }
+    ]
 ]);
 
 function uri($url): string
 {
-    $isDev = $_ENV['APP_ENV'] === 'dev';
-    $parts = Str::split($url, '.');
-    $name = $parts[0];
+    $isDev = isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'dev';
+    if ($isDev) {
+        return 'http://localhost:3000/assets/' . $url;
+    }
     $manifestFile = 'public/manifest.json';
     $manifest = json_decode((string)file_get_contents($manifestFile), true, 512, JSON_THROW_ON_ERROR);
 
-    $found = A::find($manifest, function ($item) use ($name) {
-        return $item['name'] === $name;
+    $found = A::find($manifest, function ($item, $key) use ($url) {
+        return isset($item['src']) && $item['src'] === 'assets/' . $url;
     });
     if (!$found) {
         return '';
     }
-    return $isDev ? 'http://localhost:3000/' . $found['src'] : 'public/' . $found['file'];
+    return 'public/' . $found['file'];
 }
