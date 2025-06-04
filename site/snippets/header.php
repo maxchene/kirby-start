0f2ff8e19c3a2383b04bfc1f9f748d5b9100479c
max <maxime@kaliel.fr>
2025-06-04 14:53:24 +0200

    seo title & jsonld

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var \Kirby\Cms\Site $site
+ * @var \Kirby\Cms\Page $page
  */
 ?>
 <!DOCTYPE html>
@@ -8,12 +9,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title><?= $page->seoTitle(); ?></title>
   <?php if ($site->isDev()): ?>
     <script type="module" src="http://localhost:3000/@vite/client"></script>
   <?php endif; ?>
   <?= css('css/app.scss'); ?>
   <?= js('js/app.js', ['defer' => true, 'type' => 'module']); ?>
+  <?= $page->structuredData(); ?>
 </head>
 <body>
 <aside>
@@ -29,8 +31,10 @@
     <div class="socials">
       <a href="https://www.facebook.com/brunopaquetimmobilier"
          target="_blank" title="Facebook Bruno Paquet Immobilier"><?= icon('facebook'); ?></a>
-      <a href="https://www.linkedin.com/company/bruno-paquet-immobilier" title="Linkedin Bruno Paquet Immobilier" target="_blank"><?= icon('linkedin'); ?></a>
-      <a href="https://www.instagram.com/brunopaquetimmobilier" target="_blank" title="Instagram Bruno Paquet Immobilier"><?= icon('instagram'); ?></a>
+      <a href="https://www.linkedin.com/company/bruno-paquet-immobilier" title="Linkedin Bruno Paquet Immobilier"
+         target="_blank"><?= icon('linkedin'); ?></a>
+      <a href="https://www.instagram.com/brunopaquetimmobilier" target="_blank"
+         title="Instagram Bruno Paquet Immobilier"><?= icon('instagram'); ?></a>
     </div>
   </div>
 </aside>
